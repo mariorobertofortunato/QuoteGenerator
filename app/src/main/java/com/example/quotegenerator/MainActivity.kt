c07e74ace7ac3a8b07679ef0d1059d504d067e5a
mariorobertofortunato <mariorobertofortunato@gmail.com>
2022-03-06 22:24:45 +0100

    .

--- app/src/main/java/com/example/quotegenerator/MainActivity.kt
+++ app/src/main/java/com/example/quotegenerator/MainActivity.kt
@@ -8,19 +8,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.quotegenerator.api.Network
 import com.example.quotegenerator.databinding.ActivityMainBinding
+import com.example.quotegenerator.model.Quote
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var quote = Quote ("xxx","yyy","zzz")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
 
         //Main btn click listener
         binding.mainButton.setOnClickListener {
@@ -33,16 +37,24 @@ class MainActivity : AppCompatActivity() {
         binding.infoButton.setOnClickListener {
             displayDialog()
         }
-
     }
 
     private suspend fun refreshQuote() {
-        val quote = Network.retrofitService.getQuote()
-        binding.mainTextView.text = quote
+        //JSONString from API method
+        val jsonString = Network.retrofitService.getQuote()
+        //Convert JSONString in JSONArray
+        val jsonArray = JSONArray(jsonString)
+        //Convert JSONArray (in pos x) in JSONObject
+        val jsonObject: JSONObject = jsonArray.getJSONObject(0)
+        // From the jObject assigns the string labelled "q" (the actual quote) or "a" (author)
+        // to the same field of the Kotlin Quote object
+        // and bind the string to the view
+        binding.mainTextView.text = jsonObject.getString("q")
+        binding.authorTextView.text = jsonObject.getString("a")
     }
 
-
     //Display dialog with info
+    //TODO fare intent a web o mail?
     private fun displayDialog() {
         val builder = AlertDialog.Builder(this)
             .setTitle(resources.getString(R.string.dialog_title))
---
+++ app/src/main/java/com/example/quotegenerator/model/Quote.kt
@@ -0,0 +1,11 @@
+package com.example.quotegenerator.model
+
+/**The model for the Kotlin data object
+ * q = quote text
+ * a = author
+ * h = boh*/
+
+
+
+
+data class Quote (var q: String, var a: String, var h: String)
