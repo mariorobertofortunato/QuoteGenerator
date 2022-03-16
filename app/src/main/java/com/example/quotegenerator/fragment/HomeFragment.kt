ade669073a1e4c796f0978c6edba6ff5f77dd89c
Mario Roberto Fortunato <mariorobertofortunato@gmail.com>
2022-03-16 01:15:51 +0100

    .

--- app/src/main/java/com/example/quotegenerator/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/quotegenerator/fragment/HomeFragment.kt
@@ -1,22 +1,28 @@
 package com.example.quotegenerator.fragment
 
 import android.Manifest
-import android.os.Build
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.quotegenerator.R
 import com.example.quotegenerator.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
 
 class HomeFragment : Fragment() {
 
@@ -40,15 +46,13 @@ class HomeFragment : Fragment() {
         /** LISTENERS */
 
         binding.picture.setOnClickListener {
-            Toast.makeText(requireContext(),"Ciao",Toast.LENGTH_SHORT).show()
-            //TODO work in progress
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-                //askPermissions()
-            } else {
-                CoroutineScope(Dispatchers.Main).launch {
-                    //downloadImage(viewModel.getPictureUrl())
+            if (isPermissionGranted()) {
+                Toast.makeText(requireContext(),"Downloading picture",Toast.LENGTH_SHORT).show()
+                lifecycleScope.launch {
+                    downloadImage(viewModel.getPictureUrl())
                 }
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
             }
         }
 
@@ -75,17 +79,45 @@ class HomeFragment : Fragment() {
 
     private fun animation () {
         binding.container.alpha = 0f
-        binding.container.translationY = 50f
-        binding.container.animate().alpha(1f).translationY(-100f).duration = 1000
+        binding.container.translationY = 4000f
+        binding.container.animate().alpha(1f).translationY(-1f).duration = 1000
     }
 
-    private suspend fun getPicture() {
-        Picasso.get()
-            .load(viewModel.getPictureUrl())
-            .placeholder(R.drawable.ic_baseline_error_24)
-            .error(R.drawable.ic_baseline_error_24)
-            .into(binding.picture)
-    }
+    /**PICTURE METHODS*/
+
+            private suspend fun getPicture() {
+                Picasso.get()
+                    .load(viewModel.getPictureUrl())
+                    .placeholder(R.drawable.ic_baseline_error_24)
+                    .error(R.drawable.ic_baseline_error_24)
+                    .into(binding.picture)
+            }
+
+            private fun downloadImage (url: String) {
+                //TODO salvare immagine in galleria
+            }
+
+    /**PERMISSION METHODS*/
+
+            private fun isPermissionGranted() : Boolean {
+                return checkSelfPermission(requireActivity(),Manifest.permission.WRITE_EXTERNAL_STORAGE) === PermissionChecker.PERMISSION_GRANTED
+            }
+
+            override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+                if (requestCode == 1) {
+                    if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                        //downloadImage(viewModel.getPictureUrl())
+                        Toast.makeText(requireContext(),"aaaaaaa",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Snackbar.make(binding.mainLayout, "Authorization needed", Snackbar.LENGTH_LONG
+                        ).setAction("Settings") {
+                            startActivity(Intent(Settings.ACTION_SETTINGS))
+                        }
+                            .show()
+
+                    }
+                }
+            }
 
 
 }
